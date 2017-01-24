586e7c8f8bb31ac2cda521cd4f3b3f711d695e39
Dan Loaiza <dan.loaiza@easytaxi.com.br>
2017-01-24 18:50:31 -0500

    update scale conversion method for correct precision

--- src/utils.lisp
+++ src/utils.lisp
@@ -5,8 +5,8 @@
 
 (in-package :utils)
 
-(defun convert_scale (val &key (minor 3) (maximum 21))
-  (let* ((m (min maximum val))
-        (res (- m 10)))
+(defun convert_scale (val &key (minor 3) (maximum 14))
+  (let* ((d (floor (/ maximum 2)))
+         (res (- val d)))
     (max minor res)))
 
